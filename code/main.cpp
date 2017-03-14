ae98e58c7652fe2ec655238650c786c113936faf
Loïc Messal <loic.messal@orange.fr>
2017-03-14 12:58:14 +0100

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -8,8 +8,8 @@
 #include <GLFW/glfw3.h>
 
 // GL includes
-#include "Shader.h"
 #include "Model.h"
+#include "Shader.h"
 
 // GLM Mathemtics
 #include <glm/glm.hpp>
@@ -17,209 +17,277 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include "AprilTagReader.h"
+#include <vector>
 
 // Callback de fermeture de la fenêtre
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void key_callback(GLFWwindow *window, int key, int scancode, int action,
+                  int mode);
 
-int main()
-{
-    /************ Initialisation Fenetre, Shaders, texture ***********/
-    
-    GLuint screenWidth = 800, screenHeight = 600;
-    
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    
-    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "OpenGL", nullptr, nullptr); // Windowed
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
+int main() {
+  /************ Initialisation Fenetre, Shaders, texture ***********/
 
-   
-    glewExperimental = GL_TRUE;
-    glewInit();
-    
-    // Define the viewport dimensions
-    int width, height;
-    // On recupere les dimensions de la fenetre creee plus haut
-    glfwGetFramebufferSize(window, &width, &height);
-    glViewport(0, 0, width, height);
-    
-    glEnable(GL_DEPTH_TEST);
+  GLuint screenWidth = 800, screenHeight = 600;
 
-    Shader shader("opengl_code/shaders/default.vertexshader", "opengl_code/shaders/default.fragmentshader");
-    Shader lightshader("opengl_code/shaders/light.vertexshader", "opengl_code/shaders/light.fragmentshader");
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLuint texture; // Declaration de l'identifiant
+  GLFWwindow *window = glfwCreateWindow(screenWidth, screenHeight, "OpenGL",
+                                        nullptr, nullptr); // Windowed
+  glfwMakeContextCurrent(window);
+  glfwSetKeyCallback(window, key_callback);
 
-	glGenTextures(1, &texture); // Generation de la texture
-	// On bind la texture cree dans le contexte global d'OpenGL
-	glBindTexture(GL_TEXTURE_2D, texture); 
-	// Modification des parametres de la texture
-	// Methode de wrapping
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); 
-	// Methode de filtrage
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glewExperimental = GL_TRUE;
+  glewInit();
 
-    // On unbind la texture
-	glBindTexture(GL_TEXTURE_2D, 0);
-	
-    
-    /*********************************************************************/
-    
-    /****************** Initialisation de l'image 2D *********************/
+  // Define the viewport dimensions
+  int width, height;
+  // On recupere les dimensions de la fenetre creee plus haut
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
 
-	GLfloat vertices[] = {
-        /*     Positions    |      Normales     |     UV     */
-       1.0f,  1.0f, 0.0f,   0.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
-       1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
-      -1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 0.0f,   0.0f, 0.0f, // Bottom Left
-      -1.0f,  1.0f, 0.0f,   0.0f, 0.0f, 0.0f,   0.0f, 1.0f  // Top Left
-    };
-    
-    GLshort indices[]{
-    	0, 1, 3,
-    	1, 2, 3
-    };    
-    
-    GLuint VBO, VAO, EBO;
-    
-    glGenVertexArrays(1, &VAO);
-    
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    
-    glBindVertexArray(VAO);
-    
-    // On met notre EBO dans le contexte global
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    // On assigne au EBO le tableau d'indices
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); 
+  glEnable(GL_DEPTH_TEST);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  Shader shader("opengl_code/shaders/default.vertexshader",
+                "opengl_code/shaders/default.fragmentshader");
+  Shader lightshader("opengl_code/shaders/light.vertexshader",
+                     "opengl_code/shaders/light.fragmentshader");
 
-    // Attribut des positions
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-    // Attribut des normales
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
-    // Attribut des UV
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6*sizeof(GLfloat)));
-    glEnableVertexAttribArray(2);
-    
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    
-    /*********************************************************************/
+  GLuint texture; // Declaration de l'identifiant
 
-    /******************* Chargement des modeles 3D ***********************/
+  glGenTextures(1, &texture); // Generation de la texture
+  // On bind la texture cree dans le contexte global d'OpenGL
+  glBindTexture(GL_TEXTURE_2D, texture);
+  // Modification des parametres de la texture
+  // Methode de wrapping
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // Methode de filtrage
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-    // ...
-    
-    /*********************************************************************/
-    
-    /******************* Initialisation du detecteur *********************/
+  // On unbind la texture
+  glBindTexture(GL_TEXTURE_2D, 0);
 
-    // Instanciation
-    AprilTagReader reader(0,640,480);
-    
-    // Récupération de la 1ere image
-    cv::Mat image = reader.getImage();
-    
-    /*********************************************************************/
-    
-    /*************** Création de la caméra virtuelle *********************/
+  /*********************************************************************/
 
-    
-    // Matrice de projection
-    glm::mat4 projection = glm::mat4(1.0f);
-    
-    // Matrice de position de la caméra
-    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+  /****************** Initialisation de l'image 2D *********************/
 
-    // Envoie de la matrice de projection aux shaders
+  GLfloat vertices[] = {
+      /*     Positions    |      Normales     |     UV     */
+      0.5f,  0.375f,  0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top Right
+      0.5f,  -0.375f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, // Bottom Right
+      -0.5f, -0.375f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Bottom Left
+      -0.5f, 0.375f,  0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f  // Top Left
+  };
+
+  GLshort indices[]{0, 1, 3, 1, 2, 3};
+
+  GLuint VBO, VAO, EBO;
+
+  glGenVertexArrays(1, &VAO);
+
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  // On met notre EBO dans le contexte global
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  // On assigne au EBO le tableau d'indices
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Attribut des positions
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+                        (GLvoid *)0);
+  glEnableVertexAttribArray(0);
+  // Attribut des normales
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+                        (GLvoid *)(3 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(1);
+  // Attribut des UV
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+                        (GLvoid *)(6 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(2);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  /*********************************************************************/
+
+  /******************* Chargement des modeles 3D ***********************/
+
+  // ...
+
+  /*********************************************************************/
+
+  /******************* Initialisation du detecteur *********************/
+
+  // Instanciation
+  AprilTagReader reader(0, 640, 480);
+
+  // Récupération de la 1ere image
+  cv::Mat image = reader.getImage();
+
+  /*********************************************************************/
+
+  /*************** Création de la caméra virtuelle *********************/
+
+  // Matrice de projection
+  glm::mat4 projection = glm::mat4(1.0f);
+
+  // Creation de la perspective
+  projection =
+      glm::perspective(0.7348556912801973f, 640.0f / 480.0f, 0.5f, 100.0f);
+
+  float focale = projection[0][0];
+  // Matrice de position de la caméra
+  glm::mat4 view =
+      glm::lookAt(glm::vec3(0.0f, 0.0f, 0.5 * focale),
+                  glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+  // Envoie de la matrice de projection aux shaders
+  shader.Use();
+  glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE,
+                     glm::value_ptr(view));
+  glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1,
+                     GL_FALSE, glm::value_ptr(projection));
+  lightshader.Use();
+  glUniformMatrix4fv(glGetUniformLocation(lightshader.Program, "view"), 1,
+                     GL_FALSE, glm::value_ptr(view));
+  glUniformMatrix4fv(glGetUniformLocation(lightshader.Program, "projection"), 1,
+                     GL_FALSE, glm::value_ptr(projection));
+
+  /*********************************************************************/
+
+  Model monObjet = Model("opengl_code/model/suzanne/suzanne.obj");
+  Model maMaison = Model("opengl_code/model/nanosuit/nanosuit.obj");
+  std::vector<TagData> listTag;
+
+  // Game loop
+  while (!glfwWindowShouldClose(window)) {
+    // Récupération des evenements et maj du fond
+    glfwPollEvents();
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_DEPTH_BUFFER_BIT);
+
+    /******************* MaJ de la texture (image) ***********************/
+
     shader.Use();
-    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
-    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-    lightshader.Use();
-    glUniformMatrix4fv(glGetUniformLocation(lightshader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
-    glUniformMatrix4fv(glGetUniformLocation(lightshader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-    
+    image = reader.getImage();
+    cv::flip(image, image, 0);
+    glActiveTexture(GL_TEXTURE0); //  Binding  de  notre  texture
+    glBindTexture(
+        GL_TEXTURE_2D,
+        texture); //  Association du numero de la texture  pour le  shader
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.cols, image.rows, 0, GL_BGR,
+                 GL_UNSIGNED_BYTE, image.ptr());
+    glUniform1i(glGetUniformLocation(shader.Program, "modelTexture"), 0);
+
     /*********************************************************************/
 
-    
-    // Game loop
-    while(!glfwWindowShouldClose(window))
-    {
-        // Récupération des evenements et maj du fond
-        glfwPollEvents();
-        glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClear(GL_DEPTH_BUFFER_BIT);
+    /********************** On positionne l'image ************************/
 
-        
-        /******************* MaJ de la texture (image) ***********************/
+    glm::mat4 model;
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+    model = glm::rotate(model, glm::pi<float>(), glm::vec3(0.0f, 1.0f, 0.0f));
+    // On passe la position de l'objet au shader
+    glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1,
+                       GL_FALSE, glm::value_ptr(model));
 
-        shader.Use();
-        image = reader.getImage();
-        cv::flip(image, image, 0);
-		glActiveTexture(GL_TEXTURE0 );//  Binding  de  notre  texture
-		glBindTexture(GL_TEXTURE_2D , texture );//  Association du numero de la texture  pour le  shader
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, image.cols, image.rows,0, GL_BGR, GL_UNSIGNED_BYTE, image.ptr());
-		glUniform1i(glGetUniformLocation(shader.Program , "modelTexture"), 0);
-        
-        /*********************************************************************/
+    /*********************************************************************/
 
-        
-        /********************** On positionne l'image ************************/
-        
-        glm::mat4 model;
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+    /************************ On dessine l'image *************************/
+
+    glBindVertexArray(VAO);
+    // On dessine l'objet courant
+    glDrawElements(GL_TRIANGLES, 3 * 2, GL_UNSIGNED_SHORT, 0);
+    glBindVertexArray(0);
+
+    /*********************************************************************/
+
+    /********************* Affichage des éléments 3D *********************/
+    glClear(GL_DEPTH_BUFFER_BIT);
+    lightshader.Use();
+    listTag = reader.getTags();
+
+    for (size_t i = 0; i < listTag.size(); i++) {
+      if (listTag[i].tagId == 0) {
+
+        model = glm::mat4(1.0);
+        std::cout << "modele modifie" << std::endl;
+
+        model[3][0] = listTag[i].position[0];
+        model[3][1] = listTag[i].position[1];
+        model[3][2] = listTag[i].position[2];
+
+        for (size_t j = 0; j < 3; j++) {
+          for (size_t k = 0; k < 3; k++) {
+            model[j][k] = listTag[i].orientation[k][j];
+          }
+        }
+
+        model = glm::scale(model, glm::vec3(5.0f, 5.0f, 5.0f));
+        model =
+            glm::rotate(model, glm::pi<float>(), glm::vec3(0.0f, 0.0f, 1.0f));
         // On passe la position de l'objet au shader
-        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(glGetUniformLocation(lightshader.Program, "model"),
+                           1, GL_FALSE, glm::value_ptr(model));
 
-        /*********************************************************************/
+        monObjet.Draw(lightshader);
+      }
+      if (listTag[i].tagId == 1) {
 
-        
-        /************************ On dessine l'image *************************/
+        model = glm::mat4(1.0);
+        std::cout << "modele modifie" << std::endl;
 
-        glBindVertexArray(VAO);
-        // On dessine l'objet courant 
-        glDrawElements(GL_TRIANGLES, 3*2, GL_UNSIGNED_SHORT, 0);
-        glBindVertexArray(0);
+        model[3][0] = listTag[i].position[0];
+        model[3][1] = listTag[i].position[1];
+        model[3][2] = listTag[i].position[2];
 
-        /*********************************************************************/
+        for (size_t j = 0; j < 3; j++) {
+          for (size_t k = 0; k < 3; k++) {
+            model[j][k] = listTag[i].orientation[k][j];
+          }
+        }
 
-        /********************* Affichage des éléments 3D *********************/
-        lightshader.Use();
+        // model = glm::scale(model, glm::vec3(5.0f, 5.0f, 5.0f));
+        model =
+            glm::rotate(model, glm::pi<float>(), glm::vec3(0.0f, 0.0f, 1.0f));
+        // On passe la position de l'objet au shader
+        glUniformMatrix4fv(glGetUniformLocation(lightshader.Program, "model"),
+                           1, GL_FALSE, glm::value_ptr(model));
 
-        // ...
-        
-        /*********************************************************************/
-    
-        // Mise à jour de l'affichage
-        glfwSwapBuffers(window);
+        maMaison.Draw(lightshader);
+      }
     }
-    
-    // Libération de la mémoire
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
-    
-    glfwTerminate();
-    return 0;
+
+    /*********************************************************************/
+
+    // Mise à jour de l'affichage
+    glfwSwapBuffers(window);
+  }
+
+  // Libération de la mémoire
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+
+  glfwTerminate();
+  return 0;
 }
 
 // Callback de fermeture de la fenêtre
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+void key_callback(GLFWwindow *window, int key, int scancode, int action,
+                  int mode) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
 }
